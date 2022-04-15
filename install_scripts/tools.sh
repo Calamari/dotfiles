83da23af3d5cb5cfc1dc43691175256b47b02a4f
Calamari <georg@tavonius.de>
2022-04-16 01:08:47 +0200

    install vscode plugins

--- install_scripts/tools.sh
+++ install_scripts/tools.sh
@@ -18,3 +18,29 @@ if [ ! -f $HOME/.rvm/bin/rvm ]; then
   echo "Installing Ruby"
   curl -sSL https://get.rvm.io | bash -s stable --ruby
 fi
+
+if command -v code &> /dev/null ; then
+  # GIT
+  code --install-extension GitHub.copilot
+  code --install-extension eamodio.gitlens
+  code --install-extension pantajoe.vscode-elixir-credo
+  code --install-extension ms-azuretools.vscode-docker
+  # Frontend
+  code --install-extension dbaeumer.vscode-eslint
+  code --install-extension esbenp.prettier-vscode
+  code --install-extension zerokol.vscode-eex-beautify
+  code --install-extension eamodio.gitlens
+  # Go
+  code --install-extension golang.go
+  # Rust
+  code --install-extension rust-lang.rust
+  code --install-extension matklad.rust-analyzer
+  # Elixir
+  code --install-extension phoenixframework.phoenix
+  # Ruby
+  code --install-extension rebornix.ruby
+  code --install-extension castwide.solargraph
+  code --install-extension misogi.ruby-rubocop
+  # YAML
+  code --install-extension redhat.vscode-yaml
+fi
